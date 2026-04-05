d65100a0700288bc7b4376c61f61d1282540300e
erk08 <erick.diego.moura@gmail.com>
2026-04-05 19:33:26 -0300

    feat: inserção do cadastro via mensagens no console

--- script.js
+++ script.js
@@ -7,6 +7,29 @@ let estoque = prompt("Qual a quantidade em estoque?")
 
 //inserção de variaveis com o prompt sempre retornando em string, tornando necessario converte-los em valor numerico 
 vinho = parseInt(vinho)
-safra = parseInt(safra)   
+safra = parseInt(safra) 
 estoque = parseInt(estoque)
 
+// verifica o conjunto valido de opcoes 
+if (vinho < 1 || vinho > 3) {
+    // Se a opção for inválida, exibe um alerta avisando o usuário
+    alert("Digite uma opção válida")
+}
+else {
+    // Se a opção for válida avisa que os dados serão exibidos no console
+    alert("As informações sobre seu vinho estão expostas na aba console!")
+}
+
+// Verifica cada opção escolhida e exibe os dados correspondentes no console
+if (vinho == 1) {
+
+    console.log(`Vinho cadastrado: Tinto\nSafra: ${safra}\nQntd no estoque: ${estoque}´)
+}
+else if (vinho == 2) {
+
+    console.log(`Vinho cadastrado: Branco\nSafra: ${safra}\nQntd no estoque: ${estoque}´)
+}
+else {
+
+    console.log(`Vinho cadastrado: Rosé\nSafra: ${safra}\nQntd no estoque: ${estoque}´)
+}
